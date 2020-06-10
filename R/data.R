f5ae0add6b5366da99164ee21d80a99d36918343
Mohammed Ali <mali@uplandsoftware.com>
2020-06-10 15:31:02 +0200

    #1, init package

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' DrugBank parsed dataset.
+#'
+#' A dataset containing 74 different datasets parsed
+#' from DrugBank xml database using dbparser r package
+#' \url{https://dainanahan.github.io/dbparser/index.html}.
+#' The package version is copying with DrugBank database
+#'  used versions:
+#'  * dbparser: 1.1.2
+#'  * Drugbank: 5.1.6
+#' @format 75 different dataframes contianing different drugs
+#' retlated data
+#' @source \url{https://www.drugbank.ca}
+NULL
