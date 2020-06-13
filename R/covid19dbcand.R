4f45c28898094a8d4197e8bddb605de2d39a5c25
Mohammed Ali <mali@uplandsoftware.com>
2020-06-13 18:50:48 +0200

    #2, add package document

---
+++ R/covid19dbcand.R
@@ -0,0 +1,18 @@
+#' covid19dbcand: A data package that contains different datasets extracted 
+#' from \strong{DrugBank} xml database.
+#'
+#' The datasets in `covid19dbcand` is related to 
+#' [DrugBank](https://www.drugbank.ca/covid-19#drugs) selected drugs for
+#' covid-19
+#' 
+#' The package is a smaller version from 
+#' [dbdataset package](https://github.com/Dainanahan/dbdataset).
+#' 
+#' It is extracted using [dbparser](https://dainanahan.github.io/dbparser/) 
+#' 
+#' For more information kindly check the
+#' reference/index (https://docs.ropensci.org/dbparser/reference/index.html)
+#'
+#' @docType package
+#' @name covid19dbcand
+NULL
